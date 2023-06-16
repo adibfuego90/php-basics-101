b0b1535537d9ac2a14c5d7241ef9711d4076a989
adibfuego90 <adiburraman11@gmail.com>
2023-06-16 22:19:24 +0600

    comment and output const and function are added

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
 <?php
 
-echo " Adibur rahman is a good man <br/> ";
+/*  Comments 3 Type 
+1. // 1st process (use this for better understand of js )
+2 . # 2nd process  
+3. /* 3rd process */ 
 
-$a =90;
-echo $a;
-$arr = [23,"adib" , 23];
-var_dump ($arr) ;
-$b =90;
-echo $sum = $a+$b;
+//  Output function and construct 
+
+// echo ---> more than argument allow 
+// echo 12, " adibur rahman ";
+
+// print  ---> one argument allow not more than one 
+// print "Adibur Rahman";
+
+// print_r() --> more use for array 
+// print_r([12,23,12,23]);
+
+// var_dump() --> give us more details about variable 
+/* $a =90;
+var_dump($a); */
+
+// var_export() --> like var_dump() and output a string representing of a variable 
+$b =909;
+var_export($b);
+
 ?>
