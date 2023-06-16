b7682b1c8aa5dc2f6d9b46fc7f5ce384254e729c
adibfuego90 <adiburraman11@gmail.com>
2023-06-16 23:07:34 +0600

    php data type

---
+++ basic-02.php
@@ -0,0 +1,46 @@
+
+<?php
+//  variable start with "$" and at first place can not use number we have to use character
+
+// 8 type of Data in php 
+
+// String 
+/* $name = "Md. Adibur Rahman".'<br>';
+echo $name;
+echo gettype($name); */
+
+// integer 
+/* $age =90;
+echo $age; */
+
+// float
+/* $number = 90.90;
+echo "number"; */
+
+// boolean
+// $bool = true;
+
+// array
+// $array = [12,23,34];
+
+// object --> see in object oriented programming 
+
+// null
+// $nan = null;
+
+// resource --> any king of resource can be doc img and video 
+
+
+// note : single quote can not perse variable we have to use double quote(" ")  ex: 
+
+// $a = 90;
+// echo 'my age is $a';  --> shows us my age is $a;
+
+// echo "my age is $a"; correctly worked 
+
+
+
+
+
+
+?>
