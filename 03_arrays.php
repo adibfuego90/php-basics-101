60dfd0600fb1c1f16fd79d5f015e3389188b2aea
adibfuego90 <adiburraman11@gmail.com>
2023-06-17 08:12:47 +0600

    array access and associative & multidimesional array

--- 03_arrays.php
+++ 03_arrays.php
@@ -11,16 +11,48 @@ echo $numbers[1]; //access the array  */
 $fruits = array("banana","mango","apple","orange");
 print_r ($fruits); */
 
-// Associative array 
+// Associative array : 
+
 /* $fr =[
   1 => "apple",
   23 => "banana"   //not use this kind of array 
 ];
 echo $fr[2]; */
 
-$hex = [
+/* $hex = [
   "red" => "#ff0000",
-  "orange" => "#ffA500"
+  "orange" => "#ffA500"     //it can use in DB or like that 
 
 ];
-print_r($hex);
+print_r($hex); */
+/* 
+$person = [
+  "first_name" => "Md. Adibur Rahman",
+  "last_name" => "Adib",
+  "email" => "adiburraman11@gmail.com"
+];
+echo $person["email"]; //output : adiburramna11@gmail.com */
+
+
+// Multidimensional array 
+
+/* $people = [
+  [
+  "first_name" => "Md. Adibur Rahman",
+  "last_name" => "Adib",
+  "email" => "adiburraman11@gmail.com"
+  ],
+  [
+  "first_name" => "Md. Muhibur Rahman",
+  "last_name" => "muhin",
+  "email" => "muhinraman11@gmail.com"
+  ],
+  [
+  "first_name" => "Shamima Akter",
+  "last_name" => "sheuly",
+  "email" => "sheuly11@gmail.com"
+]
+];
+echo $people [1]["last_name"]; // output : muhin;
+var_dump(json_encode($people));  //it will change it to the json format 
+ */
