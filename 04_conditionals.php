8b9e6337788b263083315df62138ede20a7e2d61
adibfuego90 <adiburraman11@gmail.com>
2023-06-17 11:57:07 +0600

    Switch case is done

---
+++ 04_conditionals.php
@@ -0,0 +1,77 @@
+<?php
+/* 
+// < less than 
+// > Grater Than 
+// <= less than or equal to
+// >= Grater than or equal to 
+// == equal to 
+// === Identical to 
+// != Not equal to 
+// !== Not Identical to */
+
+/*  -------------------If Statement  -----------------*/
+// $age =19;
+
+/* if($age>=18){
+  echo "You are old enough to Vote";
+}else {
+  echo "You can not 😌";
+} */
+
+// $date = date("F j");  //date() function use for date information like month and date and year etc; F= month , j=date , H = hours 
+// echo $date ;
+
+/* $t = date("H");
+if($t < 12 ){
+echo "Good Morning ☕";
+}else if ($t <17){
+  echo "Good Afternoon ! ";
+}else {
+  echo "Good Evening 🍇";
+} */
+
+
+// $posts = ["Js oop", "CSS new update", "Html 6 is bad or good"];
+
+/* if (!empty($posts)){
+  echo $posts[2];
+}else {
+  echo "No Post available";
+} */
+
+// ternary 
+// echo !empty($posts) ? $posts[2] : "No Post available";
+
+/* // coalescing operator 
+$first_post = $posts[0] ?? null;
+echo $first_post;  */
+
+
+// Switch Condition 
+
+/* $fav_color = "red";
+
+switch($fav_color){
+  case "red" :
+    echo 'Your Favorite Color is Red';
+    break;
+  case "green" :
+    echo 'Your Favorite Color is Green';
+    break;
+  case "Black" :
+    echo 'Your Favorite Color is Black';
+    break;
+  default : 
+    echo "Your Favorite color is mot Green,Red & Black 😑";
+} */
+
+
+
+
+
+
+
+
+
+
+?>
