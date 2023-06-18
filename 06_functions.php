9c8663035d4b0a78fe95cab4bb364f49515d07b5
adibfuego90 <adiburraman11@gmail.com>
2023-06-18 08:14:22 +0600

    function done

--- 06_functions.php
+++ 06_functions.php
@@ -1,9 +1,37 @@
 <?php
+/* // $x=10 ."<br>"; //Global Variable
 function registerUser(){
+  // global $x; // we have to use global when working with global variable or anything from global scoop;
+  // echo $x;
   echo "user Registration";
 }
 
-registerUser();
+registerUser(); */
+
+
+/* function login($email){
+  echo $email." ". "Registered";
+}
+login("adibur11@gmail.com"); */
+
+
+/* ------------------------------------------- */
+
+/* function sum($n1,$n2){
+  return $n1+$n2;
+};
+echo sum(12,23); */
+
+
+/* 
+// Anonymous function 
+$subtract = function($x,$y){
+  return $x - $y;
+};
+echo $subtract(12,13); 
+*/
+
+
 
 
 ?>
