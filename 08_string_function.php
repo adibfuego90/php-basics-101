62fb1c75c5ee8c0dc3533edcbd0f497a323ea84a
adibfuego90 <adiburraman11@gmail.com>
2023-06-18 22:17:14 +0600

    String function basic

---
+++ 08_string_function.php
@@ -0,0 +1,21 @@
+<?php
+
+$string = "Md. Adibur Rahman";
+
+// Get the length of a string
+// echo strlen($string);
+
+// find the position on first occurrence of a substring
+// echo strpos($string,"A");
+
+// find the position on last occurrence of a substring
+// echo strrpos($string,'m');
+
+// Reverse the String 
+// echo strrev($string);
+
+// convert all character to the uppercase 
+// echo strtoupper($string);
+
+// convert all character to the uppercase 
+// echo strtolower($string);
