4e7cdcc87e7a4249016d4e1509fe061591cbdbae
adibfuego90 <adiburraman11@gmail.com>
2023-06-18 22:51:35 +0600

    String function is done

--- 08_string_function.php
+++ 08_string_function.php
@@ -17,5 +17,32 @@ $string = "Md. Adibur Rahman";
 // convert all character to the uppercase 
 // echo strtoupper($string);
 
-// convert all character to the uppercase 
+// convert all character to the lowercase 
 // echo strtolower($string);
+
+// Uppercase the first character of each word 
+// echo ucwords($string);
+
+// string replace 
+// echo str_replace("Md","MD",$string);
+
+// Return a portion of a string 
+// echo substr($string,0,2);
+// echo substr($string,2);  //return what come after that position 
+
+// Start with 
+/* if(str_starts_with($string,"Md")){
+  echo "Yes";
+} */
+
+// End with 
+/* if (str_ends_with($string,"Rahman")){
+  echo "Yes";
+} */
+
+/* $string2 = "<h1> Hello</h1>";
+echo htmlspecialchars($string2); */
+
+// printf("%s love %s","Adib","math");
+printf("1+1=%d", 1+1);
+
