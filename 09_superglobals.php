738a3f8323d1787f33b8aaae25ffad12afdb7a19
adibfuego90 <adiburraman11@gmail.com>
2023-06-19 21:29:46 +0600

    SupperGlobal variable

---
+++ 09_superglobals.php
@@ -0,0 +1,37 @@
+<?php
+
+/* ---------- Super Globals----------- */
+// SuperGlobals is build in variable that are always available in all scopes 
+
+/*
+  $GLOBALS - A superglobal variable that holds information about any variables in global scope.
+  $_GET - Contains information about variables passed through a URL or a form.
+  $_POST -  Contains information about variables passed through a form.
+  $_COOKIE - Contains information about variables passed through a cookie.
+  $_SESSION - Contains information about variables passed through a session.
+  $_SERVER - Contains information about the server environment.
+  $_ENV - Contains information about the environment variables.
+  $_FILES -  Contains information about files uploaded to the script.
+  $_REQUEST - Contains information about variables passed through the form or URL.
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+    <li>Host: <?php echo $_SERVER["HTTP_HOST"]; ?></li>
+    <li>Document Root : <?php echo $_SERVER["DOCUMENT_ROOT"]; ?></li>
+    <li>Server Name : <?php echo $_SERVER["SERVER_NAME"]; ?></li>
+    <li>Server port : <?php echo $_SERVER["SERVER_PORT"]; ?></li>
+    <li>Current file dir : <?php echo $_SERVER["PHP_SELF"]; ?></li>
+    <li>Request URI : <?php echo $_SERVER["REQUEST_URI"]; ?></li>
+    
+  </ul>
+</body>
+</html>
