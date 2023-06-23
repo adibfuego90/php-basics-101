100b55d8f9f652f66f5ce5d00867937e801f96e0
adibfuego90 <adiburraman11@gmail.com>
2023-06-23 11:34:55 +0600

    how to destroy any session

--- extra/dashboard.php
+++ extra/dashboard.php
@@ -7,3 +7,6 @@ if(isset($_SESSION['username'])){
   echo '<h1>Welcome guest !</h1>';
   echo '<a href="/php-basics-101/13_session.php">Home</a>';
 };
+
+
+// if you want to destroy th session we must use the : session_destroy()
